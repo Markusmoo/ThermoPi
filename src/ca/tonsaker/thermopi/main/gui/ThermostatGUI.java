6019cfbade3299ca70b8cb888cd1620be6072952
Markus Tonsaker <markusmoo@users.noreply.github.com>
2017-04-02 17:39:10 -0700

    Update 0.6a -Began implementation of Thermostat

---
+++ src/ca/tonsaker/thermopi/main/gui/ThermostatGUI.java
@@ -0,0 +1,13 @@
+package ca.tonsaker.thermopi.main.gui;
+
+import javax.swing.*;
+
+/**
+ * Created by Marku on 2017-04-02.
+ */
+public class ThermostatGUI {
+    private JPanel thermostatPanel;
+    private JButton decreaseButton;
+    private JButton increaseButton;
+    private JLabel setToVar;
+}
